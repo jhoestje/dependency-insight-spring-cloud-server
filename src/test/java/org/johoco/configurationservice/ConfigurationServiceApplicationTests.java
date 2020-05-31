be0b9808043d6ede15aa235332e9fe93e751a5bf
John Hoestje <john.hoestje+GITHUB@gmail.com>
2020-05-30 20:37:51 -0500

    basic project files

---
+++ src/test/java/org/johoco/configurationservice/ConfigurationServiceApplicationTests.java
@@ -0,0 +1,19 @@
+package org.johoco.configurationservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+//	  @Autowired
+//	  private MyService myService;
+//
+//	  @Test
+//	  public void contextLoads() {
+//	    assertThat(myService.message()).isNotNull();
+//	  }
+//
+//	  @SpringBootApplication
+//	  static class TestConfiguration {
+//	  }
+}
